d4a6eddced7d2913fed67006aab4dfef9cc37cbc
Abdirizak Abdullahi Hussein <cuzeyr6793@gmail.com>
2026-03-03 22:53:37 +0300

    feat(watch): add functions and tests for retrieving recent and popular episodes and promos

---
+++ watch/watch.go
@@ -0,0 +1,82 @@
+package watch
+
+import (
+	"github.com/abdirizak-alaaja/jikan-gin-api/common"
+	"github.com/abdirizak-alaaja/jikan-gin-api/helper"
+)
+
+// WatchEpisodes struct
+type WatchEpisodes struct {
+	Data []struct {
+		Entry    common.EntryTitle3 `json:"entry"`
+		Episodes []struct {
+			MalId   int    `json:"mal_id"`
+			Url     string `json:"url"`
+			Title   string `json:"title"`
+			Premium bool   `json:"premium"`
+		} `json:"episodes"`
+		RegionLocked bool `json:"region_locked"`
+	} `json:"data"`
+	Pagination common.Pagination `json:"pagination"`
+}
+
+// GetWatchRecentEpisodes returns watch recent episodes
+func GetWatchRecentEpisodes() (*WatchEpisodes, error) {
+	res := &WatchEpisodes{}
+	err := helper.UrlToStruct("/watch/episodes", res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+// GetWatchPopularEpisodes returns watch popular episodes
+func GetWatchPopularEpisodes() (*WatchEpisodes, error) {
+	res := &WatchEpisodes{}
+	err := helper.UrlToStruct("/watch/episodes/popular", res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+// WatchPromos struct
+type WatchPromos struct {
+	Data []struct {
+		Title   string             `json:"title"`
+		Entry   common.EntryTitle3 `json:"entry"`
+		Trailer struct {
+			YoutubeId string `json:"youtube_id"`
+			Url       string `json:"url"`
+			EmbedUrl  string `json:"embed_url"`
+			Images    struct {
+				ImageUrl        string `json:"image_url"`
+				SmallImageUrl   string `json:"small_image_url"`
+				MediumImageUrl  string `json:"medium_image_url"`
+				LargeImageUrl   string `json:"large_image_url"`
+				MaximumImageUrl string `json:"maximum_image_url"`
+			} `json:"images"`
+		} `json:"trailer"`
+	} `json:"data"`
+	Pagination common.Pagination `json:"pagination"`
+}
+
+// GetWatchRecentPromos returns watch recent promos
+func GetWatchRecentPromos() (*WatchPromos, error) {
+	res := &WatchPromos{}
+	err := helper.UrlToStruct("/watch/promos", res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+// GetWatchPopularPromos returns watch popular promos
+func GetWatchPopularPromos() (*WatchPromos, error) {
+	res := &WatchPromos{}
+	err := helper.UrlToStruct("/watch/promos/popular", res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
